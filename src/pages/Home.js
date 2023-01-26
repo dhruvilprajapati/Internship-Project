e8a2a6f2567c192c77ce9ef15c4d8f0c4bb3f076
Prajapati Dhruvil <107102870+dhruvilprajapati@users.noreply.github.com>
2023-01-26 11:12:49 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,14 +8,14 @@ export default function Layout() {
         <img src={avtar} width={180} height={180} alt="Dhruvil Prajapati" className='rounded-circle zoom member' />
         <h3 className='mt-5 textanimation'>I'm Dhruvil Prajapati</h3>
         <h4 className='mt-3'>Software Developer</h4>
-        <p className="text-center border-bottom pb-3 mt-3">I am a hard working, honest individual. I am a good timekeeper, always willing to learn new skills. I am friendly, helpful and polite, have a good sense of humour. I am able to work independently in busy environments and also within a team setting. I am outgoing and tactful, and able to listen effectively when solving problems.</p>
+        <p className="text-center border-bottom pb-3 mt-3">I am a hard working, honest individual. To continuously enhance my knowledge, skills and experience by getting involved in challenging work environment and utilize them for personal and organizational growth to the best of my ability.</p>
         <p className="text-center" style={{paddingTop: "35px"}}>
           <span className="d-mobile-block">Email: prajapatidhruvil.a@gmail.com</span>
           <br />
           <br/>
           <span className="d-mobile-block">Phone: +91-9099270401</span>
         </p>
-        <p className="text-center">Address: 44,Swaminarayan Park-1, Vastral, Ahmedabad. IN - 382418</p>
+        <p className="text-center">Address: Ahmedabad. INDIA</p>
       </div>
     </div>
   )
